1f636f42fab1aaae343636f56de1af0b2d6c89e1
Vinay Kumar <vinaysikhera2203@gmail.com>
2024-12-21 23:21:30 +0530

    first node modules

---
+++ day1/division.js
@@ -0,0 +1,9 @@
+function division(a, b) {
+    if (b == 0) {
+        return console.error('error');
+    } else {
+        return (a / b)
+    }
+    // return a / b;
+}
+module.exports = {division};
---
+++ day1/index.js
@@ -0,0 +1,25 @@
+// index.js
+const { sum } = require("./sum");
+const { multiplication } = require("./multiplication");
+const { subtraction } = require('./subtraction');
+const { division } = require('./division');
+
+let sumA = 3;
+let sumB = 5;
+let sumResult = sum(sumA, sumB);
+console.log('run ', sumResult)
+
+let mulA = 4;
+let mulB = 6;
+let mulResult = multiplication(mulA, mulB);
+console.log(mulResult);
+
+let sabA=9;
+let subB=3;
+let sub=subtraction(sabA,subB);
+console.log(sub);
+
+let divA=9;
+let divB=0;
+let divs=division(divA,divB);
+console.log(divs);
